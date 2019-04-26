d4529a956b0f65c75cedbefb1b247f3aa4cab768
Keith Burgie <krburgie@gmail.com>
2019-04-26 15:15:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 def prime?(input)
   
+  # Not prime if 1, 0, or negative
   if input <= 1
     return false
     
   else
     range = (2..input).to_a
+    
+  # remove the last item, aka input
+  # so that input % input never occurs
     range.pop
 
     range.each do |i|
