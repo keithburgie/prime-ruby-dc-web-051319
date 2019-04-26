57c5f7c6fa61ebe7c663698c9981acdfa4044605
Keith Burgie <krburgie@gmail.com>
2019-04-26 15:10:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,19 @@
 def prime?(input)
   
-  range = (2..input).to_a
-  range.pop
-
-  range.each do |i|
-    if input % i != 0
-      #do nothing
+  if input < 0 = return false
     else
-      return false
-    end
+      range = (2..input).to_a
+      range.pop
+
+      range.each do |i|
+        if input % i != 0
+          #do nothing
+        else
+          return false
+        end
+      end
+      return true
+  
   end
-  
-  return true
-  
+    
 end
