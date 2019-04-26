0e80794efd695043f90541fd99a2b25d0dcc307f
Keith Burgie <krburgie@gmail.com>
2019-04-26 15:02:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,16 @@
-def prime?(num)
+def prime?(input)
   
-  return false if num <= 1
-  return true if num == 2 || 3
-  
-  range = (3..num).to_a
-  
+  range = (2..input).to_a
+  range.pop
+
   range.each do |i|
-    return false if num % i == 0
-    return true if num % i != 0
+    if input % i != 0
+      #do nothing
+    else
+      return false
+    end
   end
   
+  return true
+  
 end
-
-prime?(2)
-prime?(3)
-prime?(4)
-prime?(5)
-prime?(1763)
