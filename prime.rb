7d799fa82ee54ae3d5f69f5e98f4e0f3b6c3e6fb
Keith Burgie <krburgie@gmail.com>
2019-04-26 14:42:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  
+  return false if num <= 1
+  return true if num == 2 || 3
+  
+  range = (3..num).to_a
+  
+  range.each do |i|
+    return false if num % i == 0
+    return true if num % i != 0
+  end
+  
+end
+
+prime?(2)
+prime?(3)
+prime?(4)
+prime?(5)
+prime?(1763)
